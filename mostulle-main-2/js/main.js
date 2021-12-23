22acefbbaafc2d96eb1e63676d275847ec1d347c
eqodel <86313716+eqodel@users.noreply.github.com>
2021-12-23 16:30:16 +0300

    Add files via upload

---
+++ mostulle-main-2/js/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+  $('#js-carousel-1').owlCarousel();
+});
+
+$('#js-carousel-1').owlCarousel({
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplaySpeed: 3000,
+  autoplayHoverPause: true,
+  slideTransition: 'linear',
+  items: 1,
+  nav: false,
+  dots: false,
+
+  responsive: {
+    600: {
+      items: 3
+    }
+  }
+});
+
+var menuButton = $('.menu');
+menuButton.on('click', function () {
+  $('.nav-wrap').toggleClass('nav-wrap_visible');
+});
+
+AOS.init();
+
+$('.footer__form').each(function() {
+    $(this).validate({
+    messages: {
+    name: "Пожалуйста, оставьте ваше имя*",
+    phone: "Также ваш номер телефона*",
+    email: {
+      required: "We need your email address to contact you",
+      email: "Your email address must be in the format of name@domain.com"
+    }
+  }
+  });
+  });
+
+  $(document).ready(function(){
+    $('.phone').mask('+7 (999) 999-99-99');
+  });
+
+  var worksSlider = new Swiper('.works__slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+function slowScroll(id) {
+  var offset = 0;
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - offset
+  }, 1000);
+  return false;
+}
